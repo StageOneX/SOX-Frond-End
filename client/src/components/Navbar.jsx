34d8c9b06696c63940be50fbec08f4beaeb8fd26
Deshapri <champika1019@gmail.com>
2025-09-25 20:09:30 +0530

    tes18

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,30 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Navbar.css';
+import React, { useEffect } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  useEffect(() => {
+    const hero = document.querySelector(".navbar");
+    const handleMouseMove = (e) => {
+      const x = (e.clientX / window.innerWidth) * 100;
+      const y = (e.clientY / window.innerHeight) * 100;
+      hero.style.setProperty("--x", `${x}%`);
+      hero.style.setProperty("--y", `${y}%`);
+    };
+    document.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      document.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        {/* Logo */}
-        <div className="navbar-logo">
-          <h2>StageOneX</h2>
+
+        {/* Left - Logo */}
+        <div className="navbar-left">
+          <div className="navbar-logo">
+            <h2>StageOneX</h2>
+          </div>
         </div>
 
-        {/* Menu Links */}
-        <ul className="navbar-links">
-          <li><a href="/Home">Home</a></li>
-          <li><a href="/WhatsOn">WhatsOn</a></li>
-          <li><a href="/events">Events</a></li>
-          <li><a href="/tickets">Tickets</a></li>
-          <li><a href="/about">About Us</a></li>
-        
+        {/* Center - Links */}
+        <div className="navbar-center">
+          <ul className="navbar-links">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#WhatsOn">WhatsOn</a></li>
+            <li><a href="#events">Events</a></li>
+            <li><a href="#tickets">Tickets</a></li>
+            <li><a href="#About Us">About Us</a></li>
+          </ul>
+        </div>
 
-        {/* Sign in Button */}
-        <li><div className="navbar-actions">
-          <button className="signin-btn">Sign in</button>
-        </div></li>
-        </ul>
+        {/* Right - Buttons */}
+        <div className="navbar-right">
+          <button className="secondary-btn">Register</button>
+          <button className="signin-btn">Sign In</button>
+        </div>
+
       </div>
     </nav>
   );
