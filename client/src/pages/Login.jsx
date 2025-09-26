0ccac071a83b8208b57001380fe5da61b78d7735
Anushkaishara2002 <anushkaishara456@gmail.com>
2025-09-26 14:47:42 +0530

    tst

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -5,36 +5,61 @@ import "./Login.css";
 import { Link } from "react-router-dom";
 
 const Login = () => {
-return(
-
-<section>
     
-    <div className="login-header">
-        <h2>Welcome Miracle StageX</h2>
-        <h3>Login or <Link>create an account</Link></h3>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    return (
 
-    </div>
+        <section className="login-page">
 
-    <div className="login-form">
-        <input type="email" placeholder="Email:" /><br/><br/>
-        <input type="password" placeholder="Password:"/><br/>
-        <input type="checkbox" /> I agree all statements in Terms of service
-    </div><br/>
+            <div className="login-header">
+                <h2>Welcome Miracle StageX</h2>
+                <h4>Login or <Link><u>create an account</u></Link></h4>
 
-    <div className="sign-button">
-        <input type="button" value="SIGN UP" />
-        <h6>Forgot Password</h6>
-    </div>
+            </div>
 
-    <div className="socialmedia-button">
-        <img src="/images/fb.png" alt="facebook logo" width="20" height="20"/>
-        <input type="button" value="Login  with Facebook"></input><br/>
-        <img src="/images/google.png" alt="facebook logo" width="20" height="20"/>
-        <input type="button" value="Login  with Google"></input><br/>
-    </div><br/><br/><br/><br/><br/><br/>
-    <Footer />
+            <div className="login-form">
+                <input type="email" placeholder="Email:" /><br /><br />
+                <input type="password" placeholder="Password:" /><br />
+            </div>
+            <br />
 
-   
+            <div className="agree">   <input type="checkbox" /> I agree all statements in Terms of service</div> 
+            
+            <br />
+            <div className="sign-button">
+                <input type="button" value="SIGN UP" />
+                
+            </div>
+            
+
+            <div className="socialmedia-button">
+                <button>
+                    <img src="/images/fb.png" alt="facebook logo" width="20" height="20" />
+                    Login with Facebook
+                </button>
+                
+                <p><button>
+                    <img src="/images/google.png" alt="facebook logo" width="20" height="20" />
+                    Login with Google
+                </button></p>
+                
+            </div><br /><br /><br /><br /><br /><br />
+            <Footer />
 
 
 
@@ -45,10 +70,12 @@ return(
 
 
 
-</section>
+
+
+        </section>
 
 
 
-);
+    );
 };
 export default Login;
